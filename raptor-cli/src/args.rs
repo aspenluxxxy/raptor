2220ca8aa18619c8d75ec65600580099acfeaf21
aspen <aspenuwu@protonmail.com>
2021-03-06 22:47:30 -0500

    i forgot

---
+++ raptor-cli/src/args.rs
@@ -0,0 +1,33 @@
+use std::{path::PathBuf, str::FromStr};
+use structopt::StructOpt;
+
+pub enum Compression {
+	Lzma,
+	Gzip,
+	Zstd,
+}
+
+impl FromStr for Compression {
+	type Err = String;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		match s.to_lowercase().trim() {
+			"lzma" | "xz" => Ok(Compression::Lzma),
+			"gz" | "gzip" => Ok(Compression::Gzip),
+			"zstd" => Ok(Compression::Zstd),
+			_ => Err("invalid compression type: expected lzma/xz, gz/gzip, or zstd.".into()),
+		}
+	}
+}
+
+#[derive(StructOpt)]
+pub enum CmdArgs {
+	Pack {
+		#[structopt(short = "c", long = "compress", default_value = "Compression::Lzma")]
+		compression: Compression,
+		#[structopt(parse(from_os_str))]
+		input: PathBuf,
+		#[structopt(parse(from_os_str))]
+		output: PathBuf,
+	},
+}
--- raptor-cli/src/main.rs
+++ raptor-cli/src/main.rs
@@ -1,3 +1,46 @@
+/*
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+mod args;
+
+use raptor::DebFile;
+use std::{
+	fs::File,
+	io::{BufReader, Read},
+	time::Instant,
+};
+
 fn main() {
-	println!("Hello, world!");
+	//let args = std::env::args().collect::<Vec<String>>();
+	//let path = args[1].as_str();
+
+	let mut files = Vec::with_capacity(4096);
+
+	for file in std::fs::read_dir("procursus-tests").unwrap() {
+		let file = file.unwrap();
+		if !file.file_name().to_string_lossy().ends_with(".deb") {
+			continue;
+		}
+		files.push(file.path());
+	}
+
+	let start = Instant::now();
+	let amt_files = files.len();
+	for file in files {
+		let mut v = Vec::new();
+		let mut fd = BufReader::new(File::open(&file).unwrap());
+		fd.read_to_end(&mut v)
+			.unwrap_or_else(|err| panic!("failed to read '{}': {:?}", file.display(), err));
+		DebFile::parse(&v)
+			.unwrap_or_else(|err| panic!("failed to parse '{}': {:?}", file.display(), err));
+	}
+	let time = start.elapsed();
+	println!(
+		"took {:.4} seconds to parse {} deb files",
+		time.as_millis() as f64 / 1000.0,
+		amt_files
+	);
 }
